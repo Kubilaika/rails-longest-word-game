8d409f4287fa13ccf14fa12c3dd98bf9748bdf00
loic cessot <loic.cessot@gadz.org>
2017-05-03 17:20:05 +0200

    Longuest word game

---
+++ app/helpers/batard_helper.rb
@@ -0,0 +1,2 @@
+module BatardHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get '/game' => 'batard#game'
+  get '/score' => 'batard#score'
+  root 'batard#game'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
